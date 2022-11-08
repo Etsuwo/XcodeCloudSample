a579c1e10d064f0f9f5d7dcdeb2604e6f063bc0d
Etsushi <etsushi.otani@emoshu.co.jp>
2022-11-08 15:13:44 +0900

    Initial Commit

---
+++ XcodeCloudSample/XcodeCloudSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudSampleApp.swift
+//  XcodeCloudSample
+//
+//  Created by Etsushi Otani on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
